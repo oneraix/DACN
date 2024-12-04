3f6b82adf49ef977b11452c399d0f8d5e018128b
Hieu <oneraix113@gmail.com>
2024-12-04 21:26:07 +0700

    phattrienthem

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // apps/app.js
 const express = require('express');
 const app = express();
+
+
+
 const userRoutes = require('./apps/routes/userRoutes'); // Đảm bảo đường dẫn đúng
 const homestayRoutes = require('./apps/routes/homestayRoutes'); // Đảm bảo đường dẫn đúng
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
 
 // Middleware
 app.use(express.json());  // Để xử lý dữ liệu JSON
@@ -10,6 +16,10 @@ app.use(express.json());  // Để xử lý dữ liệu JSON
 // Routes
 app.use('/api/users', userRoutes);  // Đảm bảo là /routes/userRoutes
 app.use('/api/homestays', homestayRoutes);  // Đảm bảo là /routes/homestayRoutes
+
+// Set views directory (chỉ ra nơi chứa các view)
+app.set('views', path.join(__dirname, 'views'));
+
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
